a5265dfbd964bc0d05c88a175a2df765707d422d
olivTo <olivier.torres@ymail.com>
2016-11-23 17:40:36 +0100

    module impl

---
+++ node-env.js
@@ -0,0 +1,13 @@
+exports.printMsg = function() {
+  console.log("node-env module...");
+}
+
+const isEnv = env => (process.env.NODE_ENV === env);
+const isDebug = () => process.env.DEBUG === 'true';
+
+module.exports = {
+  isTest() { return isEnv('test'); },
+  isDev() { return isEnv('development'); },
+  isProd() { return isEnv('production'); },
+  isDebug,
+};
